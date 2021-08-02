0badc061e8a6295e02a8fe0e056ef075c641850b
lynch <459467141@qq.com>
2021-08-02 20:54:31 +0800

    1

--- src/router.jsx
+++ src/router.jsx
@@ -4,7 +4,16 @@ import App from './App'
 import Admin from './admin'
 import Login from './pages/login'
 import Buttons from './pages/ui/buttons'
-import NoMatch from './pages/nomatch'
+import Modals from './pages/ui/modals'
+import Loadings from './pages/ui/loadings'
+import Notice from './pages/ui/notice'
+import Messages from './pages/ui/messages'
+import Tabs from './pages/ui/tabs'
+import Gallery from './pages/ui/gallery'
+import Carousel from './pages/ui/carousel'
+import FormLogin from './pages/form/login'
+import FormRegister from './pages/form/register'
+// import NoMatch from './pages/nomatch'
 
 export default class IRouter extends Component {
   render() {
@@ -18,8 +27,18 @@ export default class IRouter extends Component {
               render={() => (
                 <Admin>
                   <Switch>
-                    <Route path="/admin/ui/buttons" component={Buttons} />
-                    <Route component={NoMatch} />
+                    {/* <Route path="/home" component={Home} /> */}
+                    <Route path="/ui/buttons" component={Buttons} />
+                    <Route path="/ui/modals" component={Modals} />
+                    <Route path="/ui/loadings" component={Loadings} />
+                    <Route path="/ui/notification" component={Notice} />
+                    <Route path="/ui/messages" component={Messages} />
+                    <Route path="/ui/tabs" component={Tabs} />
+                    <Route path="/ui/gallery" component={Gallery} />
+                    <Route path="/ui/carousel" component={Carousel} />
+
+                    <Route path="/form/login" component={FormLogin} />
+                    <Route path="/form/reg" component={FormRegister} />
                   </Switch>
                 </Admin>
               )}
